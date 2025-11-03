b5b80ba7ab8220c1e5b480169821d8cfb0004e58
Valentina Iramain <valentinairamain2020@gmail.com>
2025-11-03 11:52:41 -0300

    creamos estructura inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <main className='container'>
+    <h1 className='text-center py-3 display-4 text-secondary'>Lista de Empleados</h1>
+    </main>
+    </>
+  )
+}
+
+export default App
